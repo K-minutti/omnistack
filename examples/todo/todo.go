bfe89276405603b509762487bfd24cd1e006c5a7
Kevin Minutti <minutti70@gmail.com>
2023-04-26 07:19:30 -0400

    Add update and delete methods

--- examples/todo/todo.go
+++ examples/todo/todo.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "io"
+    "strconv"
     "net/http"
     "encoding/json"
     "github.com/gorilla/mux"
@@ -12,6 +13,8 @@ import (
 )
 
 var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
+
+// TODO: UPDATE gorm and  go get -u gorm.io/gorm 
 
 type TodoItemModel struct {
     Id int `gorm:"primary_key"`
@@ -25,12 +28,6 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
     io.WriteString(w, `{"status": "success"}`)
 }
 
-
-func init() {
-    log.SetFormatter(&log.TextFormatter{})
-    log.SetReportCaller(true)
-}
-
 func CreateItem(w http.ResponseWriter, r *http.Request) {
     description := r.FormValue("description")
     todo := &TodoItemModel{Description: description, Completed: false}
@@ -42,15 +39,66 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(result.Value)
 }
 
-func UpdateItem(w http.ResponseWriter, r *http.Request) {}
-func GetItemByID(Id int) bool {return true}
+func UpdateItem(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, _ := strconv.Atoi(vars["id"])
+
+    err := GetItemByID(id)
+    if err == false {
+        w.Header().Set("Content-Type", "application/json")
+        io.WriteString(w, `{"updated": false, "error": "Item not found"}`
+    } else {
+        completed, _ := strconv.ParseBool(r.FormValue("completed")
+        log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
+        todo := &TodoItemModel{}
+        db.First(&todo, id)
+        todo.Completed = completed
+        db.Save(&todo)
+        w.Header().Set("Content-Type", "application/json")
+        io.WriteString(w, `{"updated": true}`)
+    }
+}
+
+func GetItemByID(Id int) bool {
+    todo := &TodoItemModel{}
+    result := db.First(&todo, Id)
+    if result.Error != nil{
+        log.Warn("TodoItem not found in database")
+        return false
+    return true
+}
+
 func GetCompletedItems(w http.ResponseWriter, r *http.Request) {}
 func GetIncompletedItems(w http.ResponseWriter, r *http.Request) {}
-// func GetTodoItems(completed bool) interface{} {}
-func DeleteItem(w http.ResponseWriter, r *http.Request) {}
 
+func GetTodoItems(completed bool) interface{} {
+    var todos []TodoItemModel
+    TodoItems := db.Where("completed = ?", completed).Find(&todos).Value
+    return TodoItems
+}
 
+func DeleteItem(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    id, _ strconv.Atoi(vars["id"])
 
+    err := GetItemByID(id)
+    if err == false {
+        w.Header().Set("Content-Type", "application/json")
+        io.WriteString(w, `{"updated": false, "error": "Item not found"}`
+    } else {
+        log.WithFields(log.Fields{"Id": id}).Info("Deleting TodoItem")
+        todo := &TodoItemModel{}
+        db.First(&todo, id)
+        db.Delete(&todo)
+        w.Header().Set("Content-Type", "application/json")
+        io.WriteString(w, `{"deleted": true}`)
+    }
+}
+
+func init() {
+    log.SetFormatter(&log.TextFormatter{})
+    log.SetReportCaller(true)
+}
 
 func main() {
     defer db.Close()
